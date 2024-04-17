fc4fcd4749205e9e5c3ab04ad1d3ce32997d3eed
Corrin Lakeland <lakeland@gmail.com>
2024-04-17 06:03:37 +0000

    added user class

---
+++ virtual_assistant/db_setup.py
@@ -0,0 +1,11 @@
+# db_setup.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from models import Base
+
+engine = create_engine('sqlite:///virtual_assistant.db')
+Base.metadata.create_all(engine)  # Assuming your models are defined in 'models.py'
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
+    print("Database setup complete.")
